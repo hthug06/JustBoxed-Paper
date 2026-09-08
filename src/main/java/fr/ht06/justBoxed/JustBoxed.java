3db462681cc54264e91bea79a9abfa1805f3158d
ht06 <hthug06@gmail.com>
2026-09-08 21:25:02 +0200

    fix: teleport player to a fallback world when the server stop

--- src/main/java/fr/ht06/justBoxed/JustBoxed.java
+++ src/main/java/fr/ht06/justBoxed/JustBoxed.java
@@ -11,6 +11,10 @@ import fr.ht06.justBoxed.Storage.BoxRepository;
 import fr.ht06.justBoxed.Storage.DatabaseManager;
 import io.papermc.paper.command.brigadier.Commands;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
@@ -86,10 +90,21 @@ public final class JustBoxed extends JavaPlugin {
     @Override
     public void onDisable() {
         if (this.boxRegistry != null) {
+            Location fallbackSpawn = Bukkit.getWorlds().getFirst().getSpawnLocation();
+
+            // remove every invitation to avoid memory leak on reload
             for (Box box : this.boxRegistry.getAllBoxes()) {
                 box.clearInvitations();
-                if (box.isWorldLoaded(this)) {
-                    box.unloadWorld(this);
+
+                // Teleport player to another world because if this is a box world,
+                // Next time they rejoint, they're going to spawn in the base world but at the coordinate of their box world
+                World world = box.getWorld(this);
+                if (world != null) {
+                    for (Player p : world.getPlayers()) {
+                        p.teleport(fallbackSpawn);
+                    }
+
+                    Bukkit.unloadWorld(world, true);
                 }
             }
         }
