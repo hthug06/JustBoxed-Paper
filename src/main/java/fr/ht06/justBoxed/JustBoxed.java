ef38eb5e35e279091f22bab3a73c856d5f68961c
ht06 <hthug06@gmail.com>
2026-09-09 13:53:09 +0200

    feat(Templates): Rework template + create nether template.

--- src/main/java/fr/ht06/justBoxed/JustBoxed.java
+++ src/main/java/fr/ht06/justBoxed/JustBoxed.java
@@ -77,9 +77,17 @@ public final class JustBoxed extends JavaPlugin {
         // On first launch, create a template world with seed 8500081009970950196 (every biome and structure in 1000 blocks)
         // It will be used to create boxes by copying it instead of generating a new world
         if (!BoxTemplate.exist(this)) {
-            getLogger().info("Template world not found, creating it...");
-            BoxTemplate.create(this);
-            getLogger().info("Template world created !");
+            getLogger().info("Some Templates worlds not found, creating them...");
+
+            getLogger().info("Creating Overworld template...");
+            BoxTemplate.create(this, World.Environment.NORMAL);
+            getLogger().info("Overworld template created !");
+
+            getLogger().info("Creating Nether template...");
+            BoxTemplate.create(this, World.Environment.NETHER);
+            getLogger().info("Nether template created !");
+
+            getLogger().info("Templates worlds are created !");
         }
     }
 
